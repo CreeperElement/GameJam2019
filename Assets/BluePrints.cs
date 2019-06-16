acd45bd60cd6fd9cce21a58218b8c378d66533ef
Seth Fenske <fenskesd@gmail.com>
2019-06-15 22:54:36 -0500

    Infinite tunnel is possible now

---
+++ Assets/BluePrints.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BluePrints : MonoBehaviour
+{
+    public static BluePrints Instance;
+
+    public GameObject _CameraPrefab;
+    public static GameObject CameraPrefab {
+        get {
+            return Instance._CameraPrefab;
+        }
+    }
+
+    public int _MAX_RENDER_LAYERS;
+    public static int MAX_RENDER_LAYERS {
+        get {
+            return Instance._MAX_RENDER_LAYERS;
+        }
+    }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+}
---
+++ Assets/FloatingMath.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingMath
+{
+    public static bool GreaterThan(float a, float b, float delta)
+    {
+        return a + delta > b || a - delta > b;
+    }
+
+    public static bool LessThan(float a, float b, float delta)
+    {
+        return a + delta < b || a - delta < b;
+    }
+
+    public static bool Equal(float a, float b, float delta)
+    {
+        return (a - b) <= delta;
+    }
+}
