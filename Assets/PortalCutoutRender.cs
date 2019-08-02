6f8fa30899edddcc30f9a762fe01da6f8fae1a73
Seth Fenske <seth.fenske@directsupply.com>
2019-08-02 14:54:31 -0500

    Able to identify 4 dots where the portal vertices are

--- Assets/PortalCutoutRender.cs
+++ Assets/PortalCutoutRender.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,30 +13,31 @@ public class PortalCutoutRender : MonoBehaviour
     private Camera cam;
     private Texture2D mask;
 
-    private void Start()
-    {
-        //mat = new Material(spriteShader);
-        cam = GetComponent<Camera>();
+    private Color[] fillColorArray;
+    private Plane clippingPlane;
 
-        mask = new Texture2D(cam.pixelWidth, cam.pixelHeight, TextureFormat.ARGB32, true);
-        for (int x = 0; x < mask.width; x++)
-            for (int y = 0; y < mask.height; y++)
-            {
-                if (Mathf.Sqrt(
-                    (x - cam.pixelWidth / 2) * (x - cam.pixelWidth)
-                    + (y - cam.pixelHeight / 2) * (y - cam.pixelHeight / 2)) > 10)
-                    mask.SetPixel(x, y, Color.white);
-                else
-                    mask.SetPixel(x, y, new Color(0, 0, 0, 0));
-            }
+    private Mesh mesh;
+
+	private void Start()
+	{
+		cam = GetComponent<Camera>();
+		mask = new Texture2D(cam.pixelWidth, cam.pixelHeight, TextureFormat.ARGB32, true);
+
+		fillColorArray = mask.GetPixels();
+		for (int i = 0; i < fillColorArray.Length; i++)
+		{
+			fillColorArray[i] = Color.black;
+		}
+		mask.SetPixels(fillColorArray);
         mask.Apply();
 
         mat.SetTexture("_Mask", (Texture)mask);
         Debug.Log($"(Mask) Height: {mask.height} Width: {mask.width}");
         Debug.Log($"(newTexture) Height: {newTexture.height} Width: {newTexture.width}");
 
-        //mat.SetTexture("_Mask", (Texture)newTexture);
-    }
+		mesh = controller.forwardPortal.GetComponent<MeshFilter>().mesh;
+		//mat.SetTexture("_Mask", (Texture)newTexture);
+	}
 
 
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
@@ -46,46 +48,54 @@ public class PortalCutoutRender : MonoBehaviour
          * define the nearClipPlane.
          */
 
-        /*
+        
         Vector3 passThroughA, passThroughB, passThroughC;
         passThroughA = transform.position + transform.forward * cam.nearClipPlane;
         passThroughB = passThroughA + transform.right;
         passThroughC = passThroughA + transform.up;
-        Plane nearClipplingPlane = new Plane(passThroughA, passThroughB, passThroughC);
+        clippingPlane = new Plane(passThroughA, passThroughB, passThroughC);
 
-        Mesh mesh = controller.forwardPortal.GetComponent<MeshFilter>().mesh;
-        Transform portalTransform = controller.forwardPortal.transform;
-        foreach(Vector3 vertex in mesh.vertices)
-        {
-            // Make a ray
-            Vector3 vertexPosition = portalTransform.TransformPoint(vertex);
-            Ray ray = new Ray(vertexPosition, transform.position - vertexPosition);
+        Transform portalTransform = controller.transform;
 
-            Vector3 origin = cam.ScreenToWorldPoint(new Vector3(0, 0, cam.nearClipPlane));
-            Vector3 upperRight = cam.ScreenToWorldPoint(new Vector3(cam.pixelWidth, cam.scaledPixelHeight, cam.nearClipPlane));
+        Vector2[] vertices = new Vector2[mesh.vertices.Length];
+        int vertexCount = 0;
 
-            // Cast ray to plane
-            float enter;
-            if(nearClipplingPlane.Raycast(ray, out enter))
-            {
-                // Find collision point
-                Vector3 collision = ray.GetPoint(enter);
+		foreach (Vector3 vertex in mesh.vertices)
+		{
+			// Get the real world position of the vector
+			Vector3 vertexPosition = portalTransform.TransformPoint(vertex);
+			// Add the location of the vector to the screen
+			vertices[vertexCount++] = cam.WorldToScreenPoint(vertexPosition);
+		}
 
-                float rightDistance = Vector3.Project(collision - origin, passThroughB - origin).magnitude;
-                float upDistance = Vector3.Project(collision - origin, passThroughC - origin).magnitude;
+        mask.SetPixels(fillColorArray);
+		const int _RADIUS = 5;
 
-                float worldWidth = (cam.ScreenToWorldPoint(new Vector3(cam.pixelWidth,0, cam.nearClipPlane)) - origin).magnitude;
-                float worldHeight = (cam.ScreenToWorldPoint(new Vector3(0, cam.pixelHeight, cam.nearClipPlane)) - origin).magnitude;
+		foreach (Vector2 point in vertices)
+		{
+			for (int x = 0; x < 2 * _RADIUS; x++)
+			{
+				for (int y = 0; y < 2 * _RADIUS; y++)
+				{
+					//Debug.Log(GetRadius(point, new Vector2(x, y)));
+					if (GetRadius(point, new Vector2((int)point.x - _RADIUS + x, (int)point.y - _RADIUS + y)) <= _RADIUS)
+					{
+						//Debug.Log(point);
+						mask.SetPixel((int)point.x - _RADIUS + x, (int)point.y - _RADIUS + y, Color.red);
+					}
+				}
+			}
+		}
+		mask.Apply();
 
-                float pixelWidth = (rightDistance / worldWidth) * cam.pixelWidth;
-                float pixelHeight = (upDistance / worldHeight) * cam.pixelHeight;
-
-                // Add vertex to mask
-            }
-        }
-        */
         //System.IO.File.WriteAllBytes(Application.dataPath + "/" + "picture5886.png", mask.EncodeToPNG
         mat.SetTexture("_Mask", (Texture)mask);
         Graphics.Blit(source, destination, mat);
     }
+
+    private static float GetRadius(Vector2 center, Vector2 position)
+    {
+	    return Vector2.Distance(center, position);
+    }
+
 }
